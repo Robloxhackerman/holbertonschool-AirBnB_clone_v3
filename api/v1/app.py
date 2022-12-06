14c011dac95d0ee9fda4feb5979ce89996a9e3d1
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-06 11:16:03 -0300

    hmmm

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,4 +15,4 @@ def teardown(exception):
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), 
-        port=int(getenv("HBNB_API_PORT", "5000")))
+        port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
